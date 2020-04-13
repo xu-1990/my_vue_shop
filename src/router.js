398cfc48ade2850c3614d3c42ab4be1844121e68
xiaolong <798821306@qq.com>
2020-04-13 12:56:18 +0800

    完成了用户列表功能

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import Users from './components/user/Users.vue'
 
 Vue.use(Router)
 
@@ -9,7 +11,12 @@ const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home', component: Home, redirect: '/welcome', children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users }
+      ]
+    }
   ]
 })
 
@@ -18,12 +25,12 @@ router.beforeEach((to, from, next) => {
   // to 将要访问的路径
   // from 代表从哪个路径跳转而来
   // next 是一个函数，表示放行
-    // next() 放行  next('/login')  强制跳转
+  // next() 放行  next('/login')  强制跳转
 
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr){
+  if (!tokenStr) {
     return next('/login')
   } else {
     next()
