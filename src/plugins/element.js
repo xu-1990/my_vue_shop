39e457399a9dcc7176176e76e7f95fcf41cd0311
xiaolong <798821306@qq.com>
2020-04-20 21:08:51 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 
 import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload } from 'element-ui'
 
+// import Timeline from './timeline/index.js'
+// import TimelineItem from './timeline-item/index.js'
+
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -38,6 +41,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+// Vue.use(Timeline)
+// Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message  //全局设置提示消息
 Vue.prototype.$confirm = MessageBox.confirm
